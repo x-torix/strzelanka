ff0bfbea68a7ce1ad12caf314e2086d590441f4b
x-torix <martyna@piwowarska.com.pl>
2023-02-27 15:26:25 +0100

    Dodaj pliki projektów.

---
+++ Assets/playercontroler.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playercontroler : MonoBehaviour
+{
+    Vector2 movementVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementVector = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * movementVector.x);
+        transform.Translate(Vector3.forward * movementVector.y * Time.deltaTime);
+    }
+    void OnMove(InputValue inputValue)
+    {
+        movementVector = inputValue.Get<Vector2>();
+
+        Debug.Log(movementVector.ToString());
+    }
+}
