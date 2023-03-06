558be8c188837dd11687c0d04fac087df115338c
x-torix <martyna@piwowarska.com.pl>
2023-03-06 15:21:52 +0100

    06.03

--- Assets/playercontroler.cs
+++ Assets/playercontroler.cs
@@ -7,10 +7,14 @@ using UnityEngine.InputSystem;
 public class playercontroler : MonoBehaviour
 {
     Vector2 movementVector;
+    public float bulletSpeed = 20f;
+    public GameObject bulletPrefab;
+    Transform bulletSpawn;
     // Start is called before the first frame update
     void Start()
     {
         movementVector = Vector2.zero;
+        bulletSpawn = transform.Find("BulletSpawn");
     }
 
     // Update is called once per frame
@@ -24,5 +28,12 @@ public class playercontroler : MonoBehaviour
         movementVector = inputValue.Get<Vector2>();
 
         Debug.Log(movementVector.ToString());
+    }
+    void OnFire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn);
+        bullet.transform.parent = null;
+        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*bulletSpeed,ForceMode.VelocityChange);
+        Destroy(bullet, 5);
     }
 }
