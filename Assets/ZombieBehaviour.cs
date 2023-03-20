d040df6ada6db1dece38b63afc0ef613e5027ff9
x-torix <martyna@piwowarska.com.pl>
2023-03-20 14:42:02 +0100

    poprawki 20.03

--- Assets/ZombieBehaviour.cs
+++ Assets/ZombieBehaviour.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class ZombieBehaviour : MonoBehaviour
 {
-    public GameObject player;
+    int hp = 10;
+
+    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,27 @@ public class ZombieBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(player.transform.position);
+        if (hp > 0)
+        {
+            transform.LookAt(player.transform.position);
 
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Destroy(collision.gameObject);
+            hp--;
+            if (hp <= 0)
+            {
+                transform.Translate(Vector3.up);
+                transform.Rotate(Vector3.right * -90);
+                GetComponent<BoxCollider>().enabled = false;
+                Destroy(transform.gameObject, 10);
+            }
+        }
     }
 }
--- Assets/playercontroler.cs
+++ Assets/playercontroler.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class playercontroler : MonoBehaviour
 {
+    float hp = 10;
     Vector2 movementVector;
-    public float bulletSpeed = 20f;
+    public float playerSpeed = 2;
+    public float bulletSpeed = 20;
     public GameObject bulletPrefab;
     Transform bulletSpawn;
+    public GameObject hpBar;
+    Scrollbar hpScrollBar;
     // Start is called before the first frame update
     void Start()
     {
         movementVector = Vector2.zero;
         bulletSpawn = transform.Find("BulletSpawn");
+        hpScrollBar = hpBar.GetComponent<Scrollbar>();
     }
 
     // Update is called once per frame
@@ -27,7 +33,6 @@ public class playercontroler : MonoBehaviour
     {
         movementVector = inputValue.Get<Vector2>();
 
-        Debug.Log(movementVector.ToString());
     }
     void OnFire()
     {
@@ -35,5 +40,24 @@ public class playercontroler : MonoBehaviour
         bullet.transform.parent = null;
         bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*bulletSpeed,ForceMode.VelocityChange);
         Destroy(bullet, 5);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+
+            hp--;
+            if (hp <= 0) Die();
+            hpScrollBar.size = hp / 10;
+            Vector3 pushVector = collision.gameObject.transform.position - transform.position;
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(pushVector.normalized * 5, ForceMode.Impulse);
+        }
+    }
+    void Die()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        transform.Translate(Vector3.up);
+        transform.Rotate(Vector3.right * -90);
+
     }
 }
